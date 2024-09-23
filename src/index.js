928fbdfd53b4d41bf59873943642708f56b94b51
Ryna <katrin_dr@naver.com>
2024-09-23 20:39:59 +0900

    feat: solve

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    // If matrix is empty or not provided, return an empty array
+    if (!matrix || matrix.length === 0) {
+        return [];
+    }
+
+    return matrix.reduce((acc, row, index) => {
+        // For odd-indexed rows, reverse the row using slice to avoid mutating original array
+        if (index % 2 !== 0) {
+            acc.push(...row.slice().reverse());
+        } else {
+            acc.push(...row);
+        }
+        return acc;
+    }, []);
+};
+
+module.exports = towelSort;
